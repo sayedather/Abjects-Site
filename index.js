f2edcd27f006f131e73c3e85c9fe2db2dfe07793
sayedather <ather90@gmail.com>
2017-07-06 02:51:57 +0300

    files added

---
+++ index.js
@@ -0,0 +1,20 @@
+$(function() {
+  
+    $('.prompt').html('[user@Abjects] # ');
+
+  // Initialize a new terminal object
+  var term = new Terminal('#input-line .cmdline', '#container output');
+  term.init();
+  
+  // Update the clock every second
+  setInterval(function() {
+    function r(cls, deg) {
+      $('.' + cls).attr('transform', 'rotate('+ deg +' 50 50)')
+    }
+    var d = new Date()
+    r("sec", 6*d.getSeconds())  
+    r("min", 6*d.getMinutes())
+    r("hour", 30*(d.getHours()%12) + d.getMinutes()/2)
+  }, 1000);
+  
+});
