e21e39d04021c6f4b10d99c74b7e1945fa5706d0
sayedather <ather90@gmail.com>
2017-07-06 20:40:51 +0300

    aded faq

--- script.js
+++ script.js
@@ -1,170 +1,52 @@
-var util = util || {};
-util.toArray = function(list) {
-  return Array.prototype.slice.call(list || [], 0);
-};
-
-var Terminal = Terminal || function(cmdLineContainer, outputContainer) {
-  window.URL = window.URL || window.webkitURL;
-  window.requestFileSystem = window.requestFileSystem || window.webkitRequestFileSystem;
-
-  var cmdLine_ = document.querySelector(cmdLineContainer);
-  var output_ = document.querySelector(outputContainer);
-
-  const CMDS_ = [
-     'clear', 'clock', 'date', 'echo', 'help', 'uname', 'connect'
-  ];
+<!DOCTYPE html>
+<html >
+<head>
+  <meta charset="UTF-8">
+  <title>Abjects IRC Network</title>
   
-  var fs_ = null;
-  var cwd_ = null;
-  var history_ = [];
-  var histpos_ = 0;
-  var histtemp_ = 0;
+  
+  
+      <link rel="stylesheet" href="css/style.css">
 
-  window.addEventListener('click', function(e) {
-    cmdLine_.focus();
-  }, false);
+  
+</head>
 
-  cmdLine_.addEventListener('click', inputTextClick_, false);
-  cmdLine_.addEventListener('keydown', historyHandler_, false);
-  cmdLine_.addEventListener('keydown', processNewCommand_, false);
-
-  //
-  function inputTextClick_(e) {
-    this.value = this.value;
-  }
-
-  //
-  function historyHandler_(e) {
-    if (history_.length) {
-      if (e.keyCode == 38 || e.keyCode == 40) {
-        if (history_[histpos_]) {
-          history_[histpos_] = this.value;
-        } else {
-          histtemp_ = this.value;
-        }
-      }
-
-      if (e.keyCode == 38) { // up
-        histpos_--;
-        if (histpos_ < 0) {
-          histpos_ = 0;
-        }
-      } else if (e.keyCode == 40) { // down
-        histpos_++;
-        if (histpos_ > history_.length) {
-          histpos_ = history_.length;
-        }
-      }
-
-      if (e.keyCode == 38 || e.keyCode == 40) {
-        this.value = history_[histpos_] ? history_[histpos_] : histtemp_;
-        this.value = this.value; // Sets cursor to end of input.
-      }
-    }
-  }
-
-  //
-  function processNewCommand_(e) {
-
-    if (e.keyCode == 9) { // tab
-      e.preventDefault();
-      // Implement tab suggest.
-    } else if (e.keyCode == 13) { // enter
-      // Save shell history.
-      if (this.value) {
-        history_[history_.length] = this.value;
-        histpos_ = history_.length;
-      }
-
-      // Duplicate current input and append to output section.
-      var line = this.parentNode.parentNode.cloneNode(true);
-      line.removeAttribute('id')
-      line.classList.add('line');
-      var input = line.querySelector('input.cmdline');
-      input.autofocus = false;
-      input.readOnly = true;
-      output_.appendChild(line);
-
-      if (this.value && this.value.trim()) {
-        var args = this.value.split(' ').filter(function(val, i) {
-          return val;
-        });
-        var cmd = args[0].toLowerCase();
-        args = args.splice(1); // Remove cmd from arg list.
-      }
-
-      switch (cmd) {
-        case 'clear':
-          output_.innerHTML = '';
-          this.value = '';
-          return;
-        case 'clock':
-          var appendDiv = jQuery($('.clock-container')[0].outerHTML);
-          appendDiv.attr('style', 'display:inline-block');
-          output_.appendChild(appendDiv[0]);
-          break;
-        case 'date':
-          output( new Date() );
-          break;
-        case 'echo':
-          output( args.join(' ') );
-          break;
-        case 'help':
-          output('<div class="ls-files">' + CMDS_.join('<br>') + '</div>');
-          break;
-        case 'uname':
-          output(navigator.appVersion);
-          break;
-        case 'connect':
-          output('Use an irc client to connect with us at irc.abjects.net');
-          break;
+<body>
+  <html>
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="chrome=1" />
+    <title>Abjects IRC Network</title>
+    <link href="https://fonts.googleapis.com/css?family=Inconsolata"
+          rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <div id="container">
+      <output></output>
+      <div id="input-line" class="input-line">
+        <div class="prompt"></div><div><input class="cmdline" autofocus /></div>
+      </div>
+    </div>
     
-      };
+  
+    <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script src="js/script.js"></script>
+    
+    <div class="clock-container">
+      <svg viewBox="0 0 100 100">
+        <circle cx="50" cy="50" r="45"/>
+        <g>
+          <rect class="hour" x="47.5" y="12.5" width="5" height="40" rx="2.5" ry="2.55" />
+          <rect class="min" x="48.5" y="12.5" width="3" height="40" rx="2" ry="2"/>
+          <line class="sec" x1="50" y1="50" x2="50" y2="16" />
+        </g>
+      </svg>
+    </div>
+    
+  </body>
+</html>
+  
+    <script src="js/index.js"></script>
 
-      window.scrollTo(0, getDocHeight_());
-      this.value = ''; // Clear/setup line for next input.
-    }
-  }
-
-  //
-  function formatColumns_(entries) {
-    var maxName = entries[0].name;
-    util.toArray(entries).forEach(function(entry, i) {
-      if (entry.name.length > maxName.length) {
-        maxName = entry.name;
-      }
-    });
-
-    var height = entries.length <= 3 ?
-        'height: ' + (entries.length * 15) + 'px;' : '';
-
-    // 12px monospace font yields ~7px screen width.
-    var colWidth = maxName.length * 7;
-
-    return ['<div class="ls-files" style="-webkit-column-width:',
-            colWidth, 'px;', height, '">'];
-  }
-
-  //
-  function output(html) {
-    output_.insertAdjacentHTML('beforeEnd', '<p>' + html + '</p>');
-  }
-
-  // Cross-browser impl to get document's height.
-  function getDocHeight_() {
-    var d = document;
-    return Math.max(
-        Math.max(d.body.scrollHeight, d.documentElement.scrollHeight),
-        Math.max(d.body.offsetHeight, d.documentElement.offsetHeight),
-        Math.max(d.body.clientHeight, d.documentElement.clientHeight)
-    );
-  }
-
-  //
-  return {
-    init: function() {
-      output('<img align="left" src="https://i.imgur.com/jSlgtv9.png" width="202" height="82" style="padding: 0px 10px 20px 0px"><h2 style="letter-spacing: 4px">Welcome to Abjects.net</h2><p>' + new Date() + '</p><p>Enter "help" for more information.</p>');
-    },
-    output: output
-  }
-};
+</body>
+</html>
